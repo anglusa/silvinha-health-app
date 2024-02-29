6e21bf692c6785a6219587b73f83fde0c6ab6679
anglusa <otavio.baroni@etec.sp.gov.br>
2024-02-29 11:40:51 -0300

    componente form do projeto

---
+++ silvinhaHealthApp/src/components/Title/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  boxTitle: {
+    alingnItens: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    },
+  textTittle: {
+    color: '#FF0043',
+    fontSize: 28,
+    fontWeight: 'bold'
+
+    },
+})
+
+export default styles;
